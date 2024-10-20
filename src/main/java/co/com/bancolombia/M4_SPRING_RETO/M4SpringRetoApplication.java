86eadbae79a097d0cb0c54fc617c1e34a0462a1a
Juan-Alvarez-DEV-BC <jucalvar@bancolombia.com.co>
2024-10-20 17:32:27 -0500

    M4-SPRING-RETO - Inicio del Proyecto

---
+++ src/main/java/co/com/bancolombia/M4_SPRING_RETO/M4SpringRetoApplication.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.M4_SPRING_RETO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4SpringRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4SpringRetoApplication.class, args);
+	}
+
+}

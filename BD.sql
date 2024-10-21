113ebd374b68d0f3f45a40affe2e6be0a846aa14
Juan-Alvarez-DEV-BC <jucalvar@bancolombia.com.co>
2024-10-21 11:14:49 -0500

    M4-SPRING-RETO - Se ajusta la estructura de la Base de Datos

--- BD.sql
+++ BD.sql
@@ -1,26 +1,26 @@
 CREATE TABLE cuentas (
-     id         SERIAL        PRIMARY KEY,
-     nro_cuenta Varchar(10)   NOT NULL,
-     titular    Varchar(255)  NOT NULL,
-     saldo      Numeric(19,2) NOT NULL
-     tipo_cuenta Varchar(10)   NOT NULL,
+     id          SERIAL        PRIMARY KEY,
+     nro_cuenta  Varchar(10)   NOT NULL,
+     titular     Varchar(255)  NOT NULL,
+     saldo       Numeric(19,2) NOT NULL,
+     tipo_cuenta Varchar(10)   NOT NULL
    );
 
-CREATE TABLE log-transacciones (
+CREATE TABLE transacciones (
     id        SERIAL         PRIMARY KEY,
     tipo      VARCHAR(50)    NOT NULL,
     valor     NUMERIC(15, 2) NOT NULL,
     fecha     TIMESTAMP      NOT NULL,
-    id_cuenta INTEGER REFERENCES cuenta(id)
+    id_cuenta INTEGER REFERENCES cuentas(id)
 );
 
-Delete From cuenta  ;
+Delete From cuentas  ;
 
 INSERT INTO cuentas (nro_cuenta, titular, saldo, tipo_cuenta)
         VALUES
-          ('1111', 'Juan Alvarez'  , 1000.50,"BASICA"),
-          ('2222', 'Tatiana Arias' , 2500.00,"PREMIUM"),
-          ('3333', 'Salome Alvarez', 1500.75,"PREMIUM")
+          ('1111', 'Juan Alvarez'  , 1000.50, 'BASICA' ),
+          ('2222', 'Tatiana Arias' , 2500.00, 'PREMIUM'),
+          ('3333', 'Salome Alvarez', 1500.75, 'PREMIUM')
 ;
 
-Select * From cuenta;
+Select * From cuentas;
